fbf19b46225741136a22166c07f26f25bb163502
Nay Oo Kyaw <nayookyaw.nok@gmail.com>
2025-09-25 11:34:47 +1200

    optimize the codes

--- bitmaphole.php
+++ bitmaphole.php
@@ -3,6 +3,8 @@
     echo "Bitmap Hole \n";
     class BitMapHole {
         private $input_str_array;
+        private $total_bitmap_hole_count = 0;
+        private $already_taken = [];
 
         public function __construct($input_str_array) {
             $this->input_str_array = $input_str_array;
@@ -20,9 +22,7 @@
 
             echo "Input Array Size: ". count($this->input_str_array) . "\n";
             // echo ($two_dimension_array[0][0]);
-            $total_bitmap_hole_count = 0;
-            $already_taken = [];
-
+            
             for ($i = 0; $i < count($this->input_str_array); $i++) {
                 // echo $two_dimension_array[$i][0] . "\n";
 
@@ -34,52 +34,70 @@
                     $right_index = $i+1;
 
                     if ($two_dimension_array[$i][$j] === "0") {
-                        if ($top_index >=0 && $bottom_index < count($two_dimension_array[$i])) {
-                            if ($two_dimension_array[$i][$top_index] === "0" && 
-                                !in_array([$i, $top_index], $already_taken)
-                            ) {
-                                $total_bitmap_hole_count ++;
-                                array_push($already_taken, [$i, $top_index]);
-                                array_push($already_taken, [$top_index, $i]);
-                            }
-                        }
+                        // if ($top_index >=0 && $top_index < count($two_dimension_array[$i])) {
+                        //     if ($two_dimension_array[$i][$top_index] === "0" && 
+                        //         !in_array([$i, $top_index], $already_taken)
+                        //     ) {
+                        //         $total_bitmap_hole_count ++;
+                        //         array_push($already_taken, [$i, $top_index]);
+                        //         array_push($already_taken, [$top_index, $i]);
+                        //     }
+                        // }
+                        $this->checkBitMap($top_index, $two_dimension_array, $i);
 
-                        if ($bottom_index >=0 && $bottom_index < count($two_dimension_array[$i])) {
-                            if ($two_dimension_array[$i][$bottom_index] === "0" &&
-                                !in_array([$i, $bottom_index], $already_taken)
-                            ) {
-                                $total_bitmap_hole_count ++;
-                                array_push($already_taken, [$i, $bottom_index]);
-                                array_push($already_taken, [$bottom_index, $i]);
-                            }
-                        }
+                        // if ($bottom_index >=0 && $bottom_index < count($two_dimension_array[$i])) {
+                        //     if ($two_dimension_array[$i][$bottom_index] === "0" &&
+                        //         !in_array([$i, $bottom_index], $already_taken)
+                        //     ) {
+                        //         $total_bitmap_hole_count ++;
+                        //         array_push($already_taken, [$i, $bottom_index]);
+                        //         array_push($already_taken, [$bottom_index, $i]);
+                        //     }
+                        // }
+                        $this->checkBitMap($bottom_index, $two_dimension_array, $i);
 
-                        if ($left_index >=0 && $left_index < count($this->input_str_array)) {
-                            if ($two_dimension_array[$i][$left_index] === "0" &&
-                                !in_array([$i, $left_index], $already_taken)
-                            ) {
-                                $total_bitmap_hole_count ++;
-                                array_push($already_taken, [$i, $left_index]);
-                                array_push($already_taken, [$left_index, $i]);
-                            }
-                        }
+                        // if ($left_index >=0 && $left_index < count($this->input_str_array)) {
+                        //     if ($two_dimension_array[$i][$left_index] === "0" &&
+                        //         !in_array([$i, $left_index], $already_taken)
+                        //     ) {
+                        //         $total_bitmap_hole_count ++;
+                        //         array_push($already_taken, [$i, $left_index]);
+                        //         array_push($already_taken, [$left_index, $i]);
+                        //     }
+                        // }
+                        $this->checkBitMap($left_index, $two_dimension_array, $i);
 
-                        if ($right_index >=0 && $right_index < count($this->input_str_array)) {
-                            if ($two_dimension_array[$i][$right_index] === "0" &&
-                                !in_array([$i, $right_index], $already_taken)
-                            ) {
-                                $total_bitmap_hole_count ++;
-                                array_push($already_taken, [$i, $right_index]);
-                                array_push($already_taken, [$right_index, $i]);
-                            }
-                        }
+                        // if ($right_index >=0 && $right_index < count($this->input_str_array)) {
+                        //     if ($two_dimension_array[$i][$right_index] === "0" &&
+                        //         !in_array([$i, $right_index], $already_taken)
+                        //     ) {
+                        //         $total_bitmap_hole_count ++;
+                        //         array_push($already_taken, [$i, $right_index]);
+                        //         array_push($already_taken, [$right_index, $i]);
+                        //     }
+                        // }
+                        $this->checkBitMap($right_index, $two_dimension_array, $i);
                     }
 
                     // echo "iterate ". $two_dimension_array[$i][$j] . "\n";
                 }
             }
-            echo "already taken array ". count($already_taken);
-            echo "\nbitmap hole count : ". $total_bitmap_hole_count;
+            echo "already taken array ". count($this->already_taken);
+            echo "\nbitmap hole count : ". $this->total_bitmap_hole_count;
+        }
+
+        private function checkBitMap($current_index, $two_dimension_array, 
+            $two_dimension_array_index) 
+        {
+            if ($current_index >= 0 && $current_index < count($two_dimension_array[$two_dimension_array_index])) {
+                if ($two_dimension_array[$two_dimension_array_index][$current_index] === "0" && 
+                    !in_array([$two_dimension_array_index, $current_index], $this->already_taken)
+                ) {
+                    $this->total_bitmap_hole_count ++;
+                    array_push($this->already_taken, [$two_dimension_array_index, $current_index]);
+                    array_push($this->already_taken, [$current_index, $two_dimension_array_index]);
+                }
+            }
         }
     }
 
